b677dc46139054bafe168aec5d9831b029790a15
shweta-prajapati04 <139861492+shweta-prajapati04@users.noreply.github.com>
2023-09-26 16:25:43 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from './Form';
+
 import Signup from './Signup';
 
 function App() {
   return (
     <div className="App">
     <center>
-    <Form></Form>
+     <Signup></Signup>
     </center>
     </div>
   );
